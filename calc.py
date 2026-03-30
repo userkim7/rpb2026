9899efcb34a809298be6a8e270901de020be0212
Percy <userkim7@snu.ac.kr>
2026-03-30 11:45:26 +0900

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,22 +1,30 @@
 def main():
-    print("Let's implement division. Type two numbers for x and y")
+    print("Let's calculate!")
     
     x = int(input("x > "))
     y = int(input("y > "))
     
+    print("%d + %d = %d" % (x, y, add(x, y)))
+    print("%d - %d = %d" % (x, y, subtract(x, y)))
+    print("%d * %d = %d" % (x, y, multiply(x, y)))
     print("%d / %d = %0.3f" % (x, y, divide(x, y)))
     
     
-def add(a, b):
+def add(a, b) -> int:
     return a + b
+
+def subtract(a, b) -> int:
+    return a - b
+
+def multiply(a, b) -> int:
+    return a * b
     
-    
-# TODO: divide() 함수 정의
-def divide(a, b):
+def divide(a, b) -> float | None:
     try:
         return a/b
-    except:
+    except ZeroDivisionError:
         print("Error: cannot divide by zero.")     
+        return None
 
 if __name__ == "__main__":
     main()
